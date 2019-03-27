6ff0f90d7dec5d58ed99d9bf1074f005dbb8eee3
Dominik Manowski <manowski@protonmail.com>
2019-03-27 14:03:46 +0100

    Add board markup to App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from "react";
-import Header from "./components/HeaderView";
+import React, { Component } from 'react'
+import Header from './components/HeaderView'
+
+import BoardView from './components/BoardView'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header title="React Memory Game" />
+        <BoardView />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
