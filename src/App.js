59b9b3922509470ec574225636bf18e258c779d4
Dominik Manowski <manowski@protonmail.com>
2019-03-27 12:59:53 +0100

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import Header from "./components/HeaderView";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        <Header title="React Memory Game" />
       </div>
     );
   }
