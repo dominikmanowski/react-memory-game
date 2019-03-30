a6f6f43d5d481106105cd214cb74a5b9e91e83bb
Dominik Manowski <manowski@protonmail.com>
2019-03-30 17:57:37 +0100

    Add background svg patterns in patterns.css

--- src/components/BoardView/BoardView.js
+++ src/components/BoardView/BoardView.js
@@ -7,6 +7,8 @@ const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
+  max-width: 890px;
+  margin: 50px auto 0;
 `;
 
 const amount = length => Array.from({ length }, (_, i) => i);
