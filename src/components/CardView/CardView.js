c3895001a19081e86dbdd7b0fa7d79726db7403f
Dominik Manowski <manowski@protonmail.com>
2019-04-01 16:38:18 +0200

    Add flipping card on click functionality

--- src/components/CardView/CardView.js
+++ src/components/CardView/CardView.js
@@ -2,38 +2,38 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const Card = styled.div`
-  perspective: 150px;
+  perspective: 1500px;
   position: relative;
   height: 150px;
   width: 150px;
   margin: 15px;
-`;
-const CardFront = styled.div`
-  height: 150px;
-  transition: all 0.8s ease;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  backface-visibility: hidden;
-  border-radius: 3px;
-  overflow: hidden;
-  box-shadow: 0 15px 40px rgba(0, 0, 0, 0.15);
-`;
 
-const CardBack = styled.div`
-  background-color: darkturquoise;
-  height: 150px;
-  transition: all 0.8s ease;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  backface-visibility: hidden;
-  border-radius: 3px;
-  overflow: hidden;
-  box-shadow: 0 15px 40px rgba(0, 0, 0, 0.15);
-  transform: rotateY(180deg);
+  .front,
+  .back {
+    height: 150px;
+    position: absolute;
+    transition: all 0.8s ease;
+    top: 0;
+    left: 0;
+    width: 100%;
+    backface-visibility: hidden;
+    border-radius: 3px;
+    overflow: hidden;
+    box-shadow: 0 15px 40px rgba(0, 0, 0, 0.15);
+  }
+
+  .front-flipped {
+    transform: rotateY(-180deg);
+  }
+
+  .back {
+    transform: rotateY(180deg);
+    background-color: darkturquoise;
+
+    &-flipped {
+      transform: rotateY(0);
+    }
+  }
 `;
 
 class CardView extends Component {
@@ -42,11 +42,22 @@ class CardView extends Component {
     isGuessed: false
   };
 
+  handleFlippingCard = () => {
+    this.setState({
+      ...this.state,
+      isFlipped: !this.state.isFlipped
+    });
+  };
+
   render() {
     return (
-      <Card>
-        <CardFront className={this.props.bg} />
-        <CardBack />
+      <Card onClick={this.handleFlippingCard}>
+        <div
+          className={`${this.props.bg} ${
+            this.state.isFlipped ? "front-flipped" : ""
+          } front`}
+        />
+        <div className={`${this.state.isFlipped ? "back-flipped" : ""} back`} />
       </Card>
     );
   }
