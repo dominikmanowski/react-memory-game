503aae52ac0bbb226260352c86a13fdfabeadb03
Dominik Manowski <manowski@protonmail.com>
2019-04-07 21:54:00 +0200

    Move styles to separate file

---
+++ src/components/CardView/CardView.styled.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  perspective: 1500px;
+  position: relative;
+  height: 150px;
+  width: 150px;
+  margin: 15px;
+
+  .front,
+  .back {
+    height: 150px;
+    position: absolute;
+    transition: all 0.8s ease;
+    top: 0;
+    left: 0;
+    width: 100%;
+    backface-visibility: hidden;
+    border-radius: 3px;
+    overflow: hidden;
+    box-shadow: 0 15px 40px rgba(0, 0, 0, 0.15);
+  }
+
+  .front-flipped {
+    transform: rotateY(-180deg);
+  }
+
+  .back {
+    transform: rotateY(180deg);
+    background-color: darkturquoise;
+
+    &-flipped {
+      transform: rotateY(0);
+    }
+  }
+`;
