165a319b1130f8fd5229a5db65f106891c8d357f
Dominik Manowski <manowski@protonmail.com>
2019-04-18 16:18:30 +0200

    Replace IMAGE_COUNT var with fetched data

--- src/components/BoardView.js
+++ src/components/BoardView.js
@@ -23,9 +23,7 @@ const randomCardIds = (cardCount, imgUrls) => {
   return shuffle(doubledArray);
 };
 
-const IMAGE_COUNT = range(85);
-
-const randomCardIds = (cardCount, imgArr = IMAGE_COUNT) => {
+const randomCardIds = (cardCount, imgArr) => {
   let array = shuffle(imgArr).splice(0, cardCount / 2);
 
   const doubledArray = array.concat(array);
